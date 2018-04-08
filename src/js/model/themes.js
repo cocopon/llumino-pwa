7b696bf0c5f0b136b7e5d48c9d541cceea80810b
cocopon <cocopon@me.com>
2018-04-08 18:33:20 +0900

    Add some themes

--- src/js/model/themes.js
+++ src/js/model/themes.js
@@ -10,16 +10,28 @@ const THEMES: Theme[] = [
 		name: 'Default',
 	}),
 	Theme.fromObject({
-		background: '#0000ff',
-		foreground: '#ffffff',
-		id: 'blue-screen',
-		name: 'Blue screen',
-	}),
-	Theme.fromObject({
 		background: '#161821',
 		foreground: '#c6c8d1',
 		id: 'iceberg',
 		name: 'Iceberg',
+	}),
+	Theme.fromObject({
+		background: '#002b36',
+		foreground: '#839496',
+		id: 'solarized',
+		name: 'Solarized dark',
+	}),
+	Theme.fromObject({
+		background: '#282828',
+		foreground: '#ebdbb2',
+		id: 'gruvbox',
+		name: 'Gruvbox',
+	}),
+	Theme.fromObject({
+		background: '#0000ff',
+		foreground: '#ffffff',
+		id: 'blue-screen',
+		name: 'Blue screen',
 	}),
 ];
 
