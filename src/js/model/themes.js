e3f62220351c20111c9cc38a57ea4a97db5ae97a
cocopon <cocopon@me.com>
2018-04-15 01:19:44 +0900

    Add some themes :)

--- src/js/model/themes.js
+++ src/js/model/themes.js
@@ -7,6 +7,20 @@ const THEMES: Theme[] = [
 		Theme.defaultObject(),
 	),
 	Theme.fromObject({
+		background: '#0c1014',
+		energies: ['#2aa889', '#c23127'],
+		foreground: '#599cab',
+		id: 'gotham',
+		name: 'Gotham',
+	}),
+	Theme.fromObject({
+		background: '#002b36',
+		energies: ['#2aa198', '#268bd2'],
+		foreground: '#839496',
+		id: 'solarized-dark',
+		name: 'Solarized dark',
+	}),
+	Theme.fromObject({
 		background: '#161821',
 		energies: ['#84a0c6', '#a093c7'],
 		foreground: '#c6c8d1',
@@ -14,18 +28,11 @@ const THEMES: Theme[] = [
 		name: 'Iceberg',
 	}),
 	Theme.fromObject({
-		background: '#002b36',
-		energies: ['#2aa198', '#268bd2'],
-		foreground: '#839496',
-		id: 'solarized',
-		name: 'Solarized dark',
-	}),
-	Theme.fromObject({
 		background: '#282828',
 		energies: ['#b8bb26', '#fb4934'],
 		foreground: '#ebdbb2',
-		id: 'gruvbox',
-		name: 'Gruvbox',
+		id: 'gruvbox-dark',
+		name: 'Gruvbox dark',
 	}),
 	Theme.fromObject({
 		background: '#0000ff',
@@ -33,6 +40,13 @@ const THEMES: Theme[] = [
 		foreground: '#ffffff',
 		id: 'blue-screen',
 		name: 'Blue screen',
+	}),
+	Theme.fromObject({
+		background: '#e1e1e1',
+		energies: ['#719899', '#9a7599'],
+		foreground: '#616161',
+		id: 'seoul256-light',
+		name: 'Seoul256 light',
 	}),
 ];
 
