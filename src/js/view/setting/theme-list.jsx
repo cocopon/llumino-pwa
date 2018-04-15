40f74772f38940c42a9b4720914b546ba75f5329
cocopon <cocopon@me.com>
2018-04-15 10:20:20 +0900

    Improve appearance of theme list item

--- src/js/view/setting/theme-list.jsx
+++ src/js/view/setting/theme-list.jsx
@@ -1,5 +1,6 @@
 // @flow
 
+import Color from 'color';
 import * as React from 'react';
 
 import ClassName from '../../misc/class-name';
@@ -14,6 +15,7 @@ type Props = {
 };
 
 const EMPTY_HANDLER = () => {};
+const ENERGY_COUNT = 5;
 
 export default class ThemeList extends React.Component<Props> {
 	constructor(props: Props) {
@@ -24,6 +26,21 @@ export default class ThemeList extends React.Component<Props> {
 
 	render() {
 		const itemElems = this.props.themes.map((theme) => {
+			const energyElems = [];
+			for (let index = 0; index < ENERGY_COUNT; index++) {
+				const e1 = Color(theme.energyColors[0]);
+				const e2 = Color(theme.energyColors[1]);
+				energyElems.push(
+					<div
+						className={itemClassName('energy')}
+						key={index}
+						style={{
+							backgroundColor: e1.mix(e2, index / (ENERGY_COUNT - 1)).string(),
+						}}
+					/>
+				);
+			};
+
 			return (
 				<div
 					className={itemClassName()}
@@ -41,13 +58,18 @@ export default class ThemeList extends React.Component<Props> {
 									backgroundColor: theme.backgroundColor,
 								}}
 							>
-								<div
-									className={itemClassName('previewText')}
-									style={{
-										color: theme.foregroundColor,
-									}}
-								>
-									{theme.name}
+								<div className={itemClassName('previewContentLayout')}>
+									<div
+										className={itemClassName('previewText')}
+										style={{
+											color: theme.foregroundColor,
+										}}
+									>
+										{theme.name}
+									</div>
+									<div className={itemClassName('energiesLayout')}>
+										{energyElems}
+									</div>
 								</div>
 							</div>
 						</div>
